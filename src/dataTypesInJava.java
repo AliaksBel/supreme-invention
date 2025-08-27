4af93a08d14ab22eaa8b823ffd7e0e63dbdbec39
Aleksandr Belazor <avbelazor@gmail.com>
2025-08-27 23:38:21 +0300

    This information about me

---
+++ src/dataTypesInJava.java
@@ -0,0 +1,12 @@
+public class dataTypesInJava {
+    public static void main(String[] args)  {
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt = 234567876;
+        long myLong = 765456789L;
+        float myFloat = 3.14f;
+        double myDouble = 3.14876567898765678987656789876567898765678;
+        boolean myBoolean = true;
+        String myChar = "A";
+        }
+    }
