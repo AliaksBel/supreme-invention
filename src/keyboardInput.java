7a3e8d7a755474b5676b3906e6b0c959e0073c0e
Aleksandr Belazor <avbelazor@gmail.com>
2025-08-28 12:31:43 +0300

    I try 4 times - I am learning to enter data from the keyboard into the program. The user will enter data from the keyboard.

--- src/keyboardInput.java
+++ src/keyboardInput.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class keyboardInput {
     public static void main(String[] args)  {
-//Scanner userInputScanner = new Scanner(System.in);
-        System.out.println("");
+        Scanner userInputScanner = new Scanner(System.in); // вызов сканера
+        System.out.println("Введите текст"); // выводит текст пользователю
+        String userInput = userInputScanner.nextLine(); // пользователь вводит значение
 
-        System.out.println();
+        System.out.println("Моя первая строка: \"" + userInput + "\"");
 
         }
     }
