ecc0decc52886c6029d2279d1d15d51a73af2d97
Aleksandr Belazor <avbelazor@gmail.com>
2025-09-02 10:40:45 +0300

    1st main tasks Conditional operators     Main task 1 - attempt 2

--- src/SimpleNumber.java
+++ src/SimpleNumber.java
@@ -2,32 +2,40 @@ import java.util.Scanner;
 public class SimpleNumber {
     public static void main(String[] args) {
         //compareTwoNumbers(); //Задача 2]
-        taskOne();
+        taskOne(); //main task1
 
     }
+
     public static void taskOne() {
-        System.out.println("Please enter your score: ");
         int score = new Scanner(System.in).nextInt();
+        while (true) {
+            System.out.println("Please enter your score: ");
+            if (score > 0 && score <= 100) {
+                break;
+            } else {
+                System.out.println("Invalid value entered. Please enter a score between 0 - 100");
+            }
+            //  if (score > 100){
+            //    System.out.println("Invalid value entered. Please enter a score between 0 - 100");
+            if (score >= 90) {
+                System.out.println("Your grade is A!");
+            } else if (score >= 80) {
+                System.out.println("Your grade is B!");
+            } else if (score >= 70) {
+                System.out.println("Your grade is C!");
+            } else if (score >= 60) {
+                System.out.println("Your grade is D!");
+            } else if (score >= 50) {
+                System.out.println("Your grade is E!");
 
+                //else if (score < 0 ) {
+                //System.out.println("Invalid value entered. Please enter a score between 0 - 100");
+            } else System.out.println("Your grade is F1");
+            System.out.println("Good by!");
         }
-        //  if (score > 100){
-        //    System.out.println("Invalid value entered. Please enter a score between 0 - 100");
-        if (score >= 90) {
-            System.out.println("Your grade is A!");
-        } else if (score >= 80) {
-            System.out.println("Your grade is B!");
-        } else if (score >= 70) {
-            System.out.println("Your grade is C!");
-        } else if (score >= 60) {
-            System.out.println("Your grade is D!");
-        } else if (score >= 50) {
-            System.out.println("Your grade is E!");
-        }
-        //else if (score < 0 ) {
-        //System.out.println("Invalid value entered. Please enter a score between 0 - 100");
-        } else System.out.println("Your grade is F1");
-        System.out.println("Good by!");
     }
+}
+/*
     {
         //Пользователь вводит в консоли число. Если оно больше 10, выдаем в консоли сообщение
         //“Число больше десяти”, если меньше – “Число меньше десяти”
@@ -43,7 +51,7 @@ public class SimpleNumber {
     }
     // Пользователь вводит в консоли два числа, выводим сообщение “первое число
     //больше или меньше, чем второе” в зависимости от результата их сравнения
-    public static void compareTwoNumbers() {
+        /*public static void compareTwoNumbers() {
         System.out.println("Программа сравнивает разные числа: ");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первой число");
@@ -58,5 +66,4 @@ public class SimpleNumber {
             System.out.println("Второе число больше первого");
         }
         System.out.println("Вы все сделали правильно, удачи на следующей задаче");//готово))
-    }
-}
+    }*/
